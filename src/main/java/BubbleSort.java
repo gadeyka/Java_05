bcfb52a7bbbadf6d7a2ed318c8e957ea75a27f35
Gordon White <gadeyka@gmail.com>
2022-11-09 14:12:14 -0800

    sort classes added

--- src/main/java/BubbleSort.java
+++ src/main/java/BubbleSort.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class BubbleSort {
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static int[] bubbleSort(int[] array) {
+        boolean isSorted = false;
+
+        while (!isSorted) {
+
+            isSorted = true;
+
+            for (int i = 1; i < array.length; i++) {
+                if (array[i - 1] > array[i]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+
+                    isSorted = false;
+                    System.out.println(Arrays.toString(array));
+                    System.out.println(isSorted);
+                }
+                System.out.println(i);
+            }
+
+            System.out.println(" ! " + isSorted);
+        }
+
+        System.out.println("Final result: " + Arrays.toString(array));
+        return array;
+    }
 }
--- src/main/java/SortStringArray.java
+++ src/main/java/SortStringArray.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class SortStringArray {
+public class SortStringArray {
+
+    public static String[] sort(String[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if(array[i].trim().length() == 1 && array[i - 1].trim().length() == 1) {
+                    char charI = array[i].trim().charAt(0);
+                    char charIMinus1 = array[i - 1].trim().charAt(0);
+                    char charILC = array[i].trim().toLowerCase().charAt(0);
+                    char charIMinus1LC = array[i - 1].trim().toLowerCase().charAt(0);
+                    if (charILC == charIMinus1LC && charI < charIMinus1
+                            || charILC < charIMinus1LC) {
+                        String temp = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = temp;
+                        isSorted = false;
+                    }
+                } else {
+                    isSorted = true;
+                }
+            }
+        }
+        return array;
+    }
 }
